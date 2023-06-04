d49513cefdf8b4d9b3bc184b4a4381b98348d5d8
Ray Garcia <ray@slashbin.us>
2023-06-04 13:04:32 -0500

    removing unused fields in model

--- sure_path_models/models.py
+++ sure_path_models/models.py
@@ -5,11 +5,6 @@ from pydantic import BaseModel
 
 class ChangeRequest(BaseModel):
     request_number: str = None
-    description: str = None
-    backout_plan: str = None
-    test_plan: str = None
-    title: str = None
     start_time: datetime = None
     end_time: datetime = None
-    requestor: str = None
     status: str = "Unknown"
